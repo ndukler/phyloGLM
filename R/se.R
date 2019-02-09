cf9c5bf89d54902eade2480ab20f5c2241e6ebaa
Unknown <ned36@cornell.edu>
2019-02-09 12:03:30 -0500

    Improving se functionality

--- R/se.R
+++ R/se.R
@@ -1,29 +1,39 @@
 #' Compute standard errors for parameters
 #'
 #' Estimates the standard error for all non-fixed parameters in the model using the hessian
-#' @param obj rateModel
+#' @param x parameter vector or rateModel object
+#' @param hess hessian matrix for (not supplied if x is a rateModel object)
 #' @name se
 #' @return a data.table with the parameter values and standard errors
-#' @include rateModel-class.R
 #' @export 
-methods::setGeneric("se", function(obj) {
+methods::setGeneric("se", function(x,hess=NULL) {
   standardGeneric("se")
 })
 
 #' @name se
 #' @rdname se
 #' @aliases se,rateModel,rateModel-method 
-methods::setMethod("se", signature(obj = "rateModel"), function(obj) {
+methods::setMethod("se", signature(x = "rateModel",hess="missing"), function(x,hess=NULL) {
   ## Get parameter values
   paramVals=getParams(obj)[which(!obj@fixed)]
   ## Compute the hessian
   hess=numDeriv::hessian(func = phyloGLM:::scaledLL,x=paramVals,obj=obj,scale=1)
+  out=se(paramVals,hess)
+  return(out)
+})
+
+#' @name se
+#' @rdname se
+#' @aliases se,rateModel,rateModel-method 
+methods::setMethod("se", signature(x = "numeric",hess="matrix"), function(x,hess) {
+  ## Get parameter values
+  paramVals=x
   ## Invert the hessian
   covM<-solve(-hess)
   stdErr<-sqrt(diag(covM))
   ## Construct the out table to return
   temp=rbind(data.table::data.table(obj@phylogeny$getRateIndex(),pType="rate"),
-        data.table::data.table(obj@phylogeny$getPiIndex(),pType="pi"))
+             data.table::data.table(obj@phylogeny$getPiIndex(),pType="pi"))
   ## Adjust idx to idx+1 to make 1-based
   out=temp[,.(group,name,idx=idx+1,pType)]
   ## Get params
