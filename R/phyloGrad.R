f38df662e5a43bf317c8f17c9ee60b994dd57da3
Noah Dukler <ned36@cornell.edu>
2019-03-01 17:04:01 -0500

    Numerical improvements

--- R/phyloGrad.R
+++ R/phyloGrad.R
@@ -22,23 +22,30 @@ methods::setMethod("phyloGrad", signature(x="missing",model = "rateModel"), func
   x=getParams(model)
   ## Calculate stepsize for optimal finite gradient approximation
   h=abs((eps^(1/3))*x)
-  ## Set lower bound on stepsize at 10^-11
-  h[h<10^-11]=10^-11
+  ## Set lower bound on stepsize at 10^-14
+  h[h<10^-14]=10^-14
   ## Initialize likelihood vectors
   f_plus=numeric(length(x))
   f_minus=numeric(length(x))
+  ## Initialize vector for realized h
+  h_real=numeric(length(h))
   for(p in 1:length(x)){
+    ## Compute alternate values parameters
+    temp_plus=x[p]+h[p]
+    temp_minus=x[p]-h[p]
+    ## Compute realized h
+    h_real[p]=temp_plus-temp_minus
     ## Do plus calculation
-    setParams(model,x[p]+h[p],p-1)
+    setParams(model,temp_plus,p-1)
     f_plus[p]=model@phylogeny$ll(model@alleleData$alleleData@data@x,model@rateDM@x,model@piDM@x,scale,threads)
     ## Minus calculation
-    setParams(model,x[p]-h[p],p-1)
+    setParams(model,temp_minus,p-1)
     f_minus[p]=model@phylogeny$ll(model@alleleData$alleleData@data@x,model@rateDM@x,model@piDM@x,scale,threads)
     ## Reset parameter
     setParams(model,x[p],p-1)
   }
   ## Compute Gradients
-  g=(f_plus-f_minus)/(2*h)
+  g=(f_plus-f_minus)/h_real
   return(g)
 })
 
