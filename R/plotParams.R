2101910e4434f9888bdaa465420e92708ed2ae8b
Noah Dukler <ned36@cornell.edu>
2019-04-15 15:33:21 -0400

    Allow for plotting subset of parameters

--- R/plotParams.R
+++ R/plotParams.R
@@ -8,35 +8,46 @@
 #' @return a list of two plots, one for the rate parameters, one for the pi parameters
 #' @include rateModel-class.R
 #' @export
-methods::setGeneric("plotParams", function(x, hess = NULL, alpha = 0.05) {
+methods::setGeneric("plotParams", function(x, hess = NULL, alpha = 0.05, subset = NULL) {
   standardGeneric("plotParams")
 })
 
 #' @name plotParams
 #' @rdname plotParams
 #' @aliases plotParams,rateModel,rateModel-method
-methods::setMethod("plotParams", signature(x = "rateModel"), function(x, hess = NULL, alpha = 0.05) {
+methods::setMethod("plotParams", signature(x = "rateModel"), function(x, hess = NULL, alpha = 0.05, subset=NULL) {
   if (!is.null(hess) && !is.matrix(hess)) {
     stop("Hessian must be a matrix")
   }
   ## Get parameter values and standard errors
   seTab <- se(model = x, hess = hess)
   ## Create bar plot for parameters
-  gAll <- plotParams(seTab, alpha)
+  gAll <- plotParams(seTab, hess = NULL, alpha = alpha, subset = subset)
   return(gAll)
 })
 
 #' @name plotParams
 #' @rdname plotParams
 #' @aliases plotParams,data.frame,data.frame-method
-methods::setMethod("plotParams", signature(x = "data.frame"), function(x, hess = NULL, alpha = 0.05) {
+methods::setMethod("plotParams", signature(x = "data.frame"), function(x, hess = NULL, alpha = 0.05, subset = NULL) {
   ## Check if the data.frame has the correct columns
   if (!all(c("group", "name", "idx", "pType", "value", "se") %in% colnames(x))) {
     stop("Incorrectly formated parameter table. Try running se() on the rateModel object to get the correct table.")
   }
   ## Compute factor to multiply se by to compute ci
   z_star <- qnorm(p = c(alpha / 2, 1 - alpha / 2))
-
+  ## If subset isn't NULL, subset the variables that will be plotted
+  if(!is.null(subset)){
+    if(any(!subset %in% x$name)){
+      not_incl = subset[!subset %in% x$name]
+      warning(paste("Subset elements not present in model:",paste(not_incl, collapse = ",")))
+    }
+    x=x[name %in% subset,]
+    if(nrow(x)==0){
+      stop("No parameters remaining after subset")
+    }
+  }
+  
   ## Create bar plot for parameters
   pos <- ggplot2::position_dodge(width = 0.25)
   rateG <- ggplot2::ggplot(x[pType == "rate"], ggplot2::aes(
